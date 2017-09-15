098d2670cb20220073d6f3a89061d7f57a72af42
khtaaa <kahata1128@gmail.com>
2017-09-15 11:51:37 +0900

    タイトルと挿絵追加

---
+++ Assets/Scripts/start.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class start : MonoBehaviour {
+	public GameObject _Start;
+	// Alpha増減値(点滅スピード調整)
+	private float _Step = 0.01f;
+
+	void Start()
+	{
+		this._Start = GameObject.Find("start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Application.LoadLevel ("Sample");
+
+		}
+
+		// 現在のAlpha値を取得
+		float toColor = this._Start.GetComponent<Image>().color.a;
+		// Alphaが0 または 1になったら増減値を反転
+		if (toColor < 0 || toColor > 1)
+		{
+			_Step = _Step * -1;
+		}
+		// Alpha値を増減させてセット
+		this._Start.GetComponent<Image>().color = new Color(255, 255, 255, toColor + _Step);
+
+	}
+}
---
+++ Assets/moveP.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class moveP : MonoBehaviour {
+	public GameObject _Start;
+	// Alpha増減値(点滅スピード調整)
+	private float _Step = 0.01f;
+
+	void Start()
+	{
+		this._Start = GameObject.Find("start");
+	}
+
+	void Update()
+	{
+		// 現在のAlpha値を取得
+		float toColor = this._Start.GetComponent<Image>().color.a;
+		// Alphaが0 または 1になったら増減値を反転
+		if (toColor < 0 || toColor > 1)
+		{
+			_Step = _Step * -1;
+		}
+		// Alpha値を増減させてセット
+		this._Start.GetComponent<Image>().color = new Color(255, 255, 255, toColor + _Step);
+	}
+}
