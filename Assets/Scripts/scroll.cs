7fc7f002af9fa44d89084619ee2a0b1a4f0dfbf5
orika <adadad7297@yahoo.co.jp>
2017-11-01 15:00:07 +0900

    titleのスクロールスクリプト変更

--- Assets/Scripts/scroll.cs
+++ Assets/Scripts/scroll.cs
@@ -7,25 +7,24 @@ public class scroll : MonoBehaviour {
 	　public float speed = 0.1f;// スクロールスピード
 	public Camera came;
 	public int spritecount=3;
-
-	void Update () {
-		transform.localPosition += (Vector3.left+Vector3.up) * speed * Time.deltaTime;
-		}
-
-	void OnBecameInvisible()
+	public string test;
+	float hight;
+	float width;
+	void Start()
 	{
 		//スプライトの幅、高さを獲得
-		float width=GetComponent<SpriteRenderer>().bounds.size.x;
-		float hight = GetComponent<SpriteRenderer> ().bounds.size.y;
+		 width=GetComponent<SpriteRenderer>().bounds.size.x;
+		 hight = GetComponent<SpriteRenderer> ().bounds.size.y;
+	}
 
+	void Update () {
+		transform.localPosition += (Vector3.left + Vector3.up) * speed * Time.deltaTime;
 
-		if (came.transform.position.y < transform.position.y) {
-			transform.localPosition+=Vector3.down*hight*spritecount;
-		}
-
-		if(came.transform.position.x > transform.position.x)
-		{
-			transform.localPosition+=Vector3.right*width*spritecount;
+		if (came.transform.position.y + hight * 2 < transform.position.y) {
+			transform.localPosition += Vector3.down * hight * spritecount;
+		} else if (came.transform.position.x - width * 2 > transform.position.x) {
+			transform.localPosition += Vector3.right * width * spritecount;
 		}
 	}
+
 }
